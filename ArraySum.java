2f2c60be9bd61d4a6fd0e29876369f4a3d258f50
pavan-dursoju <130532707+pavan-dursoju@users.noreply.github.com>
2023-04-16 00:57:49 +0530

    Update ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -18,6 +18,9 @@ public class ArraySum
     public static void arraySum(int[] arr)
     {
         int sum=0;
+        
+        
+        
         for(int i=0;i<arr.length;i++)
         {
             sum=sum+arr[i];
